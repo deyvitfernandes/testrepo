ac348996f77e5dd5ddcb17d07e09273387fa9910
root <root@ubuntu-agent>
2017-11-29 12:06:32 +0000

    adding testrepo

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+class testrepo {
+        if $environments == 'production' {
+                notify { 'default-message' :
+                        message => "This is the production environment"
+                }
+        } else {
+                notify { 'default-message' :
+                        message => "This is not production"
+                }
+        }
+}
